ebefa76a027f9e42d378dfea85309ad42a7da3e4
PC-coding <partapchauhan8@gmail.com>
2021-02-03 00:07:51 -0500

    firebase backend setup

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyCTY-p9ASySURHMaY-HPRLKtL-hxShTn3s",
     authDomain: "picture-messaging.firebaseapp.com",
@@ -5,4 +7,13 @@ const firebaseConfig = {
     storageBucket: "picture-messaging.appspot.com",
     messagingSenderId: "983128724957",
     appId: "1:983128724957:web:95cc44f59ee5864a9c1234"
-  };
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+
+export { db, auth, storage, provider };
