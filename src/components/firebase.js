cdc8ad1cb905557b325a496fe09542ee5a3f3b61
PC-coding <partapchauhan8@gmail.com>
2021-02-03 00:03:02 -0500

    adding firebase backend

---
+++ src/components/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCTY-p9ASySURHMaY-HPRLKtL-hxShTn3s",
+    authDomain: "picture-messaging.firebaseapp.com",
+    projectId: "picture-messaging",
+    storageBucket: "picture-messaging.appspot.com",
+    messagingSenderId: "983128724957",
+    appId: "1:983128724957:web:95cc44f59ee5864a9c1234"
+  };
