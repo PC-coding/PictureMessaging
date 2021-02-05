8ae165b8b682c85b892dd84b7ddad83c0b813a49
PC-coding <partapchauhan8@gmail.com>
2021-02-05 18:09:18 -0500

    chat view properly styled

--- src/components/Chats/Chat.js
+++ src/components/Chats/Chat.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Chat.css';
 import { Avatar } from '@material-ui/core';
 import StopRoundedIcon from '@material-ui/icons/StopRounded'
+import ReactTimeago from 'react-timeago';
 
 export default function Chat({ id, username, timestamp, read, imageUrl, profilePic}){
     return(
@@ -9,7 +10,7 @@ export default function Chat({ id, username, timestamp, read, imageUrl, profileP
             <Avatar className='chat_avatar' src={profilePic}/>
             <div className='chat_info'>
                 <h4>{username}</h4>
-                <p>Tap to view - {new Date(timestamp?.toDate()).toUTCString()}</p>
+                <p>Tap to view - <ReactTimeago date={new Date(timestamp?.toDate()).toUTCString()} /> </p>
             </div>
 
             {!read && <StopRoundedIcon className='chat_readIcon'/>}
