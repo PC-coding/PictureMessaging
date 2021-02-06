582beecaa81add160519c06fe952e9f5d6f21472
PC-coding <partapchauhan8@gmail.com>
2021-02-06 14:39:03 -0500

    added exit functionality after viewing to redirect to chats

--- src/components/Chats/ChatsView.js
+++ src/components/Chats/ChatsView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { selectSelectedImage } from '../../features/appSlice';
@@ -7,6 +7,16 @@ import './ChatsView.css';
 export default function ChatsView() {
     const selectedImage = useSelector(selectSelectedImage);
     const history = useHistory();
+
+    useEffect(() => {
+        if (!selectedImage) {
+            exit();
+        }
+    }, [])
+
+    const exit = () => {
+        history.replace('/chats')
+    }
 
     return(
         <div className='chatsView'>
