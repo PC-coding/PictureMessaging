b45e4b757478c3e680230a6fa6454d6785849ee9
PC-coding <partapchauhan8@gmail.com>
2021-02-06 14:37:11 -0500

    imported useSelector, useHistory and selectSelectedImage to chatsView

--- src/components/Chats/ChatsView.js
+++ src/components/Chats/ChatsView.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { selectSelectedImage } from '../../features/appSlice';
 import './ChatsView.css';
 
 export default function ChatsView() {
+    const selectedImage = useSelector(selectSelectedImage);
+    const history = useHistory();
+
     return(
         <div className='chatsView'>
-
+            <img src={selectedImage} onClick={exit} alt='' />
         </div>
     )
 }
