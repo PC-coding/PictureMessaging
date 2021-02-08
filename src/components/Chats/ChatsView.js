3e1b49e958faf431623bcb26fc773ecace88d27e
PC-coding <partapchauhan8@gmail.com>
2021-02-07 19:47:53 -0500

    installing react countdown circle timer

--- src/components/Chats/ChatsView.js
+++ src/components/Chats/ChatsView.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { selectSelectedImage } from '../../features/appSlice';
-// import './ChatsView.css';
+import './ChatsView.css';
 
 export default function ChatsView() {
     const selectedImage = useSelector(selectSelectedImage);
