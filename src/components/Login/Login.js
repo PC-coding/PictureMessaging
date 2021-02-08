ed0ef829f36d587eec23507e0cf1246439cda230
PC-coding <partapchauhan8@gmail.com>
2021-02-08 14:24:54 -0500

    added sign in functionality with error handling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,26 @@
-import { Button } from '@material-ui/core';
 import React from 'react';
+import { Button } from '@material-ui/core';
 import './Login.css';
 import Daco from '../../Daco_4047867.png'
+import { useDispatch } from 'react-redux';
+import { auth, provider } from '../firebase';
+import { login } from '../../features/appSlice';
 
-export default function Login() {
-    const signIn = () => {};
+export default function Login({ user }) {
+    const dispatch = useDispatch();
+
+    const signIn = () => {
+        auth.signInWithPopup(provider).then((result) => {
+            console.log(user);
+            dispatch(
+                login({
+                    username: result.user.displayName,
+                    profilePic: result.user.photoURL,
+                    id: result.user.uid,
+                })
+            );
+        }).catch(error => alert(error.message));
+    };
     return(
         <div className='login'>
             <div className='loginContainer'>
