2a23cb8983a1c65d2edd50dc90ff327c5b5c3010
PC-coding <partapchauhan8@gmail.com>
2021-02-09 14:22:20 -0500

    styling done, added usernames

--- src/components/Webcam/Preview.js
+++ src/components/Webcam/Preview.js
@@ -48,7 +48,7 @@ export default function Preview(){
                 .then((url) => {
                     db.collection('posts').add({
                         imageUrl: url,
-                        username: 'Partap',
+                        username: user.username,
                         read: false,
                         profilePic: user.profilePic,
                         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
